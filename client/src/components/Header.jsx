f8b6bc095b5a6de8d6af3a446cff5cb77ff5a028
Божидара Божанова <101105150+BojidaraBojanova@users.noreply.github.com>
2024-07-05 13:56:10 +0300

    Created react project and Added Header component

---
+++ client/src/components/Header.jsx
@@ -0,0 +1,33 @@
+import logo from '../assets/images/logo.gif'
+
+export default function Header(){
+    return(
+        <header className="section site-header">
+        <div className="wrapper">
+            <h6 className="site-logo">
+                <a href="#">
+                    {/* <img src={logo} alt="logo" /> */}
+                </a>
+            </h6>
+
+            <nav className="search-nav">
+                <form action="" method="GET">
+                    <input type="text" name="searchQuery" placeholder="Search..."/>
+                    <button type="submit"><i class="fa-solid fa-magnifying-glass"></i></button>
+                </form>
+            </nav>
+
+            <nav className="main-nav">
+                <ul>
+                    <li><a href="">Home</a></li>
+                    <li><a href="">Films</a></li>
+                    <li><a href="">TV Show</a></li>
+                    <li><a href="">User</a></li>
+                    <li><a href="">Login</a></li>
+                    <li><a href="">Registration</a></li>
+                </ul>
+            </nav>
+        </div>
+    </header>
+    )
+}
