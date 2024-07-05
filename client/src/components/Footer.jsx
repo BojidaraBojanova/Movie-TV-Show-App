fb55be6f17bb987ba465a8ff1087f7a17467eb07
Божидара Божанова <101105150+BojidaraBojanova@users.noreply.github.com>
2024-07-05 14:02:41 +0300

    Added Footer

---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,9 @@
+export default function Footer(){
+    return(
+        <footer className="section site-footer">
+            <div className="wrapper">
+                <p>&copy; 2024 Movies/TV Show App. All Rights Reserved.</p>
+            </div>
+        </footer>
+    )
+}
