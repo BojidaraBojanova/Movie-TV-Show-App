b600cd41413859a84ed5b25a57b1c2219c014e3d
Божидара Божанова <101105150+BojidaraBojanova@users.noreply.github.com>
2024-07-08 17:37:31 +0300

    Updated server

--- server/config.js
+++
@@ -1,5 +0,0 @@
-const SECRET_KEY = 'jdsnjjwjdnjn8475834u8384rhfjnskfmjdn';
-
-module.exports = {
-    SECRET_KEY
-}
--- server/services/userService.js
+++
@@ -1,6 +0,0 @@
-const User = require('../models/User');
-const jwt = require('jsonwebtoken');
-const bcrypt = require('bcrypt');
-const { SECRET_KEY } = require('../config');
-
-exports.getAllUsers = () => User.find();
