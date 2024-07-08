0e6806f7c87a3f54ea48cd1800c70e5d319f965d
Божидара Божанова <101105150+BojidaraBojanova@users.noreply.github.com>
2024-07-08 23:16:11 +0300

    Created Login Form

--- client/src/components/login/Login.jsx
+++ client/src/components/login/Login.jsx
@@ -1,7 +1,29 @@
-export default function Login(){
-    return(
-        <>
-            <h1>Login Page</h1>
-        </>
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+
+export default function Login() {
+    return (
+        <div className='form-wrapper'>
+            <h1>Login</h1>
+            <Form className='form-container'>
+                <Form.Group className="mb-3" controlId="formBasicEmail">
+                    <Form.Label>Email address</Form.Label>
+                    <Form.Control type="email" className='input' placeholder="Enter email" />
+                    <Form.Text className="text">
+                        We'll never share your email with anyone else.
+                    </Form.Text>
+                </Form.Group>
+
+                <Form.Group className="mb-3" controlId="formBasicPassword">
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control type="password" className='input' placeholder="Password" />
+                </Form.Group>
+
+                <Button type="submit" className='login-btn'>
+                    Submit
+                </Button>
+            </Form>
+        </div>
+
     )
 }
